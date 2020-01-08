9e631d13353d8133a857fc82b36407b85947673f
Ihor <granovskiy07@gmail.com>
2020-01-08 20:15:42 +0200

    some new changes

--- src/test/java/test1Test.java
+++ src/test/java/test1Test.java
@@ -1,15 +1,15 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class test1 {
+public class test1Test {
     @Test
-    public void testtest(){
+    public void testTest(){
         System.out.println("****************true********************");
         Assert.assertEquals(true, true);
     }
 
     @Test
-    public void testtest2(){
+    public void testTest2(){
         System.out.println("*******************false*****************");
         Assert.assertEquals(true, false);
     }
